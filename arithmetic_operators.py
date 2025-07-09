ab897000986e5fa6aaecaba5c296e754c5c3f173
Dipesh Thakur <letsmaildipesh1@gmail.com>
2025-07-09 11:49:53 +0545

    Add code to understand the basics of arithmetic operators, string and boolean
    It contains snippets to understand the concepts

---
+++ arithmetic_operators.py
@@ -0,0 +1,15 @@
+"""
+1. ** (binding right to left)
+2. *, /, //, %
+3. +, -
+"""
+# binding determines the order of computations performed by some operations with equal priority, put side by side in one expression
+
+x = 2**3**2 # output: 512 unlike 64
+print(x)
+
+ex_1 = 6/4
+ex_2 = 6//4 #rounds the decimal down to closet integer
+ex_3 = 6 % 4
+
+print(f"Normal division:{ex_1}",f"Floor division: {ex_2}",f"Modulus Operator: {ex_3}",sep="\n")
---
+++ string_boolean_basics.py
@@ -0,0 +1,5 @@
+print(True>False) # basically python does 1 > 0
+print(True<False)
+
+print("I like \"Monty Python\"")
+print('I like "Monty Python"')
